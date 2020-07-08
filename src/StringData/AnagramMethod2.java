cc02125d2638e0276dd3f24ed48d3f1c676aeadc
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-07-08 11:15:39 +0530

    Anagram ques added with mathod 2 Optimised

--- src/StringData/AnagramMethod2.java
+++ src/StringData/AnagramMethod2.java
@@ -8,23 +8,47 @@ public class AnagramMethod2 {
 
         boolean isAnagram = true;
 
+//        int al[] = new int[256];
+//        int bl[] = new int[256];
+//
+//        for (char c: a.toCharArray())
+//        {
+//            int index = (int) c;
+//            al[index]++;
+//        }
+//        for (char c: b.toCharArray())                 NOT OPTIMISED
+//        {
+//            int index = (int) c;
+//            bl[index]++;
+//        }
+//
+//        for (int i = 0; i<256; i++)
+//        {
+//            if (al[i] != bl[i])
+//            {
+//                isAnagram = false;
+//                break;
+//            }
+//        }
+
+
         int al[] = new int[256];
-        int bl[] = new int[256];
+ //       int bl[] = new int[256];
 
         for (char c: a.toCharArray())
         {
             int index = (int) c;
             al[index]++;
         }
-        for (char c: b.toCharArray())
+        for (char c: b.toCharArray())               // OPTIMISED
         {
             int index = (int) c;
-            bl[index]++;
+            al[index]--;
         }
 
         for (int i = 0; i<256; i++)
         {
-            if (al[i] != bl[i])
+            if (al[i] != 0)
             {
                 isAnagram = false;
                 break;
